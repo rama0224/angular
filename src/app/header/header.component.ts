2a850b1abdd86838d511f8f5887fe89de0084073
rama0224 <ramakrishna022004@gmail.com>
2024-08-17 14:40:21 +0530

    angular form created

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent 
+{
+name="MBU   "
+total:number=72
+curdate:Date=new  Date()
+}
